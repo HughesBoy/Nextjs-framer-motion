8ea458c64fc8fd8d82807955d87b1fad5fe3a887
glen Hughes <gl3nhugh3s@gmail.com>
2023-04-02 11:45:16 -0700

    first animation working

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,23 @@
+"use client"
 
-import './globals.css'
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import { motion } from 'framer-motion'
+import { riseAndFade } from "@/utils/Animations";
 
 export default function Home() {
   return (
-    
-    <main className="m-6">
-      <h1 className="text-6xl font-bold">Animated header</h1>
-      <p>animated paragraph text</p>
-    </main>
-        
-  )
+    <motion.div initial="initial" animate="animate">
+      <Navbar />
+      <main className="grid grid-cols-[3fr_1fr] py-12">
+        <h1 className="text-8xl font-bold w-[40rem] leading-[90%] tracking-[-2px]">
+          Animated header
+        </h1>
+        <motion.div className="text-base leading-[150%] self-end" variants={riseAndFade}>
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout.{" "}
+        </motion.div>
+      </main>
+    </motion.div>
+  );
 }
