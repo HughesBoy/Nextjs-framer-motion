45974af3a84bff282b711e3f9ff335000518175e
glen Hughes <gl3nhugh3s@gmail.com>
2023-04-02 10:50:48 -0700

    added libraries

---
+++ src/components/AnimatedCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+//import { motion } from "framer-motion"
+
+const AnimatedCard = () => {
+  return (
+    //<motion.div animate={{ opacity: isVisible ? 1 : 0 }} />
+    <div>
+      <h1>card title</h1>
+      <p>card subtitle text</p>
+    </div>
+  )
+}
+
+export default AnimatedCard
