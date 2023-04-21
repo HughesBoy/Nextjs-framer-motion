3235bc3640ae16e123f3c39fee6ac479e3195793
glen Hughes <gl3nhugh3s@gmail.com>
2023-04-21 15:53:58 -0700

    added custom loader using nextjs13 new feature

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -2,6 +2,9 @@ import React from "react";
 async function GetRepos(){
   //https://jsonplaceholder.typicode.com/users
   const res = await fetch(`https://api.github.com/users/hughesboy/repos`);
+
+  await new Promise((resolve) => setTimeout(resolve, 1500))
+
   const repos = await res.json();
   return repos;
 }
